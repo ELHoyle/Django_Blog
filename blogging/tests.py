5edbb777655804a31cac1d9640f0730318c81929
ELHoyle <hoylee33@gmail.com>
2021-11-07 08:14:19 -0800

    blogging MVC built

--- blogging/tests.py
+++ blogging/tests.py
@@ -44,7 +44,7 @@ class FrontEndTestCase(TestCase):
             post.save()
     
     def test_list_only_published(self):
-        resp = self.client.get('')
+        resp = self.client.get('/')
         # the content of the rendered response is always a bytestring
         resp_text = resp.content.decode(resp.charset)
         self.assertTrue("Recent Posts" in resp_text)
@@ -54,6 +54,18 @@ class FrontEndTestCase(TestCase):
                 self.assertContains(resp, title, count=1)
             else:
                 self.assertNotContains(resp, title)
+
+    def test_details_only_published(self):
+        for count in range(1,11):
+            title = "Post %d Title" % count
+            post = Post.objects.get(title=title)
+            resp = self.client.get('/posts/%d/' % post.pk)
+            if count <6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
+
 
 
 # Create your tests here.
